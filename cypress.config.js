3df21e5cf2968e05176131a14b87c4a1e3fb3b7c
Erick Ribeiro <gustavoribeiroerick@gmail.com>
2023-12-01 16:57:38 -0300

    Refatorado da forma que é feito o acesso aa base url da aplicação

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://devfinance-agilizei.netlify.app',
+    
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
